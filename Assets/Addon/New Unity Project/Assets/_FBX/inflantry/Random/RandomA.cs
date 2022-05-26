fa94dac15cfa4312cb4959c864c72a0c404af852
Andrzej Sledziona <Andrzej.Sledziona@gmail.com>
2022-05-26 02:02:36 +0200

    part7

---
+++ Assets/Addon/New Unity Project/Assets/_FBX/inflantry/Random/RandomA.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomA : MonoBehaviour
+{
+    private Animator anim;
+
+    IEnumerator Start()
+    {
+        anim = GetComponent<Animator>();
+        
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+
+            anim.SetInteger("AttackIndex", Random.Range(0, 3));
+            anim.SetTrigger("Attack");
+        }
+    }
+
+   
+}
